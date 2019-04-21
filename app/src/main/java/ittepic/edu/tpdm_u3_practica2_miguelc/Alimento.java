a6d9ca4dcd22a200145dcb7ed0acb33d7710843b
MikeCervantes <jomicervantesji@ittepic.edu.mx>
2019-04-20 23:34:01 -0600

    Practica 2

---
+++ app/src/main/java/ittepic/edu/tpdm_u3_practica2_miguelc/Alimento.java
@@ -0,0 +1,11 @@
+package ittepic.edu.tpdm_u3_practica2_miguelc;
+
+public class Alimento {
+    String nombre,sabor,precio;
+
+    public Alimento(String nombre, String sabor, String precio) {
+        this.nombre = nombre;
+        this.sabor = sabor;
+        this.precio = precio;
+    }
+}
---
+++ app/src/main/java/ittepic/edu/tpdm_u3_practica2_miguelc/Bebida.java
@@ -0,0 +1,11 @@
+package ittepic.edu.tpdm_u3_practica2_miguelc;
+
+public class Bebida {
+    String nombre,sabor,precio;
+
+    public Bebida(String nombre, String sabor, String precio) {
+        this.nombre = nombre;
+        this.sabor = sabor;
+        this.precio = precio;
+    }
+}
